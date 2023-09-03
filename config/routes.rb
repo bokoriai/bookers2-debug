fc905ec45b716f9dda8b3d8fc56aae78546cfaf2
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-09-03 10:31:45 +0000

    [Update]コメント機能の追加途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
@@ -10,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorite, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
   
