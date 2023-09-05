4d74c2c7f11d26d8e37d503e1f2f6fb84857e7d9
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-09-05 04:53:54 +0000

    [Finish]応用課題4　フォローする、外すボタンの表示　フォロー一覧、フォロワー数一覧ページの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,14 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :relationships, class_name: "Relationships", foreign_key: ""
+  
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  # 一覧画面で使う
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   has_one_attached :profile_image
 
@@ -17,10 +24,25 @@ class User < ApplicationRecord
 
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no-image.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  # フォローした時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  # フォローを外す時の処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  # フォローしているかの判定
+  def following?(user)
+    followings.include?(user)
   end
   
 end
