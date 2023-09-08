30b0541f108643f0a3c692219b8ce0775a9e3689
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-09-08 10:16:49 +0000

    [Start]応用課題7

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,16 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+    let raty = new Raty(elem,opt)
+    raty.init();
+    return raty;
+}
+
 import "../stylesheets/application" 
+
+window.$ = window.jQuery = require('jquery');
 
 Rails.start()
 Turbolinks.start()
